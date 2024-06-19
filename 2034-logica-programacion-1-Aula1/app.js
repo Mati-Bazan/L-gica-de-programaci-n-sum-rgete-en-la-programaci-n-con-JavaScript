67cf5c8fab4dd6d48026fe696d7a807a45a7b12d
Matias Bazan <mati.bazan97@gmail.com>
2024-06-19 20:11:17 -0300

    Primer commit

---
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -0,0 +1,20 @@
+// alert("Hola mundo");
+let numSecreto = 6
+let numUser = prompt('Me indicas un numero por favor:')
+//alert(`El numero es: ${numUser}`)
+console.log(numUser)
+
+if (numUser == numSecreto) {
+    alert(`Acertaste, el numero secreto es: ${numSecreto}`)
+}
+
+/*
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}
+*/
