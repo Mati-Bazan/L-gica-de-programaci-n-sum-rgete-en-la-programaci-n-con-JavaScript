5e82540f563556295b469fda2b03f84d977e36bc
Matias Bazan <mati.bazan97@gmail.com>
2024-06-22 19:21:38 -0300

    Desafio 4 | FINAL aula 1

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,30 +1,40 @@
 // Variables
-let numSecreto = 6;
+let numMaxPosible = 100;
+let numSecreto = Math.floor(Math.random() * numMaxPosible) + 1;
 let numUser = 0;
 let intentos = 1;
-let palabraVeces = "vez"
+// let palabraVeces = "vez";
+let maxIntentos = 3;
+
+console.log(numSecreto)
+
 // Bucle while
 while (numUser != numSecreto) {
-    numUser = prompt("Me indicas un numero entre 1 y 10 por favor:");
+    numUser = parseInt(prompt(`Me indicas un numero entre 1 y ${numMaxPosible} por favor:`));
 
-    console.log(numUser);
+    console.log(typeof(numUser));
     /*
     Bloque de comparacion
     */
     if (numUser == numSecreto) {
         // La condicion se comple y se ejecuta el bloque
-        alert(`Acertaste, el numero secreto es: ${numSecreto}. Lo hiciste en ${intentos} ${palabraVeces}.`); // Template Strings | interpolación - `${numSecreto}`
+        alert(`Acertaste, el numero secreto es: ${numSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}.`); // Template Strings | interpolación - `${numSecreto}`
     } else {
         if (numUser > numSecreto) {
             alert("El numero secreto es menor");
         } else {
             alert("El numero secreto es mayor");
-        }
+        } // La condicion NO se comple y se ejecuta este bloque
         // Incrementamos el contador cuando no acierta 
-        intentos = intentos + 1
-        palabraVeces = "veces"
-        // La condicion NO se comple y se ejecuta este bloque
-        // alert('El numero no es correcto')
+        // intentos = intentos + 1
+        // intentos += 1
+        intentos ++
+
+        // palabraVeces = "veces"
+        if (intentos > maxIntentos) {
+            alert(`Llegaste al numero maximo de ${maxIntentos} intentos`)
+            break
+        }
     }
     console.log(`La condicion se comprobo como: ${numUser == numSecreto}`);
 }
